4409fcb19edb61e2f5b33b41df4b6e030eebf7e2
Jo Humphrey <jmh235.cam@gmail.comm>
2021-01-05 14:51:21 +0000

    Make detail slider selected option more obvious with bold text and thicker border

--- detail-slider.js
+++ detail-slider.js
@@ -5,11 +5,18 @@ document.addEventListener("DOMContentLoaded", function(){
 
     let circleMarks = document.querySelectorAll(".circle-mark"); // array
     let lines = document.querySelectorAll(".line"); // array
+    let options = document.querySelectorAll(".option"); // array
     let line1 = document.getElementById("line1");
     let line2 = document.getElementById("line2");
+    let circle1 = document.getElementById("circle1");
+    let circle2 = document.getElementById("circle2");
 
     function thicken(target) {
         target.classList.toggle("thick");
+    }
+
+    function toggleFillBackground(target) {
+        target.classList.toggle("filled");
     }
 
     function invisibiliseElement(target) {
@@ -28,19 +35,35 @@ document.addEventListener("DOMContentLoaded", function(){
         };
     };
 
+    function unfillAll(target) {
+        if ((target.classList.contains("filled"))) {
+            toggleFillBackground(target);
+        };
+    };
+
     function changeDetailLevel(event) {
         // restore everything to not-thick
         circleMarks.forEach(unthickenAll);
+        circleMarks.forEach(unfillAll);
         lines.forEach(unthickenAll);
+        options.forEach(unthickenAll);
 
         // thicken circle and corresponding lines
         thicken(event.target);
         if (event.target.id == "circle2") {
-        thicken(line1);
+            thicken(line1);
+            thicken(mark2);
+            toggleFillBackground(circle1)
         }
         else if (event.target.id == "circle3") {
-        thicken(line1);
-        thicken(line2);
+            thicken(line1);
+            thicken(line2);
+            thicken(mark3);
+            toggleFillBackground(circle1)
+            toggleFillBackground(circle2)
+        }
+        else {
+            thicken(mark1);
         }
 
         // use custom attributes to link content's id to button
